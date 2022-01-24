2708b3e7da783d5d5631582282f65764634646d5
Ivan Vasquez <ivanvasquezp@outlook.com>
2022-01-24 14:43:05 -0700

    v2.1.3
    - Added loadBackgroundImage function
    - Updated npm libraries

--- src/js/functions.js
+++ src/js/functions.js
@@ -231,3 +231,11 @@ export function saveData() {
 export function setPageBackground(url = null) {        
     document.querySelector(".page").style.backgroundImage = (url !== "" && url !== null) ? `url(${url})` : '';
 }
+export function loadBackgroundImage(element) {
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        setPageBackground(reader.result);        
+    };
+    reader.readAsDataURL(file);
+}
